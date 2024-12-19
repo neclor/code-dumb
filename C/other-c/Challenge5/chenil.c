cf3e1228f31e62c7e230907f6c8f4e16dc4d8813
neclor <neclordev@gmail.com>
2024-12-19 02:03:57 +0100

    chien

---
+++ C/other-c/Challenge5/chenil.c
@@ -0,0 +1,64 @@
+#include "chenil.h"
+
+
+int charger_chiots(char *nom_fichier, Chien *chiots, unsigned short nb_chiots) {
+	for (int i = 0; i < nb_chiots; i++) {
+		chiots[i].nom = "test";
+		chiots[i].sexe = 'M';
+		chiots[i].vaccine = 1;
+	}
+	return 1;
+}
+
+
+
+Chien *cree_chien(char *nom, char sexe, char vaccine) {
+	Chien *chien = malloc(sizeof(Chien));
+	if (!chien) return NULL;
+
+	chien->nom = nom;
+	chien->sexe = sexe;
+	chien->vaccine = vaccine;
+
+	return chien;
+}
+
+
+Portee *cree_portee(char *nom_pere, char *nom_mere, unsigned short nb_chiots, char *nom_fichier, Date date_naissance) {
+	if (!nom_pere || !nom_mere || !nom_fichier || nb_chiots < 1) return NULL;
+
+	Chien *chiots = malloc(nb_chiots * sizeof(Chien));
+	if (!chiots) return NULL;
+	if (charger_chiots(nom_fichier, chiots, nb_chiots) != 1) {
+		free(chiots);
+		return NULL;
+	}
+
+	Chien *pere = cree_chien(nom_pere, 'M', 1);
+	if (!pere) {
+		free(chiots);
+		return NULL;
+	}
+
+	Chien *mere = cree_chien(nom_mere, 'F', 1);
+	if (!mere) {
+		free(chiots);
+		free(pere);
+		return NULL;
+	}
+
+	Portee *portee = malloc(sizeof(Portee));
+	if (!portee) {
+		free(chiots);
+		free(pere);
+		free(mere);
+		return NULL;
+	}
+
+	portee->pere = pere;
+	portee->mere = mere;
+	portee->chiots = chiots;
+	portee->nb_chiots = nb_chiots;
+	portee->date_naissance = date_naissance;
+
+	return portee;}
---
+++ C/other-c/Challenge5/chenil.h
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+
+typedef struct {
+	unsigned short jour;
+	unsigned short mois;
+	unsigned short annee;
+} Date;
+
+
+typedef struct {
+	char *nom;
+	char sexe; // 'F' ou 'M'
+	char vaccine; // 0 pas vacciné, 1 vacciné
+} Chien;
+
+
+typedef struct {
+	Chien *pere;
+	Chien *mere;
+	Chien *chiots; //tableau de chiens
+	unsigned short nb_chiots;
+	Date date_naissance;
+} Portee;
+
+
+/*
+* Charge, depuis un fichier, {nb_chiots} chiots et les place dans un tableau.
+*
+* PRÉ C O N D I T I O N : {nom_fichier} valide, {chiots} est un tableau de chiens valide
+* POST C O N D I T I O N : 1 si {chiots} a bien été rempli depuis le fichier dont le
+* nom est contenu dans {nom_fichier}. -1 sinon.
+*/
+int charger_chiots(char *nom_fichier, Chien *chiots, unsigned short nb_chiots);
+
+
+/*
+* Crée un chien ayant un nom, un sexe et une information quant à sa
+* vaccination. Cette fonction alloue de la mémoire à libérer après usage.
+*
+* PR ÉCO N D I T I O N : {nom} valide, {sexe} ∈ {0M0,0 F0}, vacciné ∈ [0, 1]
+* PO S TCO N D I T I O N : Un pointeur vers un chien valide. NULL en cas d’erreur.
+*/
+Chien *cree_chien(char *nom, char sexe, char vaccine);
+
+
+/*
+* Crée une portée de chiots en fonction d’un père et d’une mère et charge,
+* depuis un fichier, les différents chiots. Cette fonction alloue de la
+* mémoire à libérer après usage.
+*
+* PR ÉCO N D I T I O N : {nom_pere} est un nom de chien valide, {nom_mere} est un nom
+* de chien valide. Le père et la mère ont été vaccinés,
+* {nb_chiots} > 0, {nom_fichier} est valide.
+* PO S TCO N D I T I O N : un pointeur vers une portée valide. Les chiots de la portée
+* ont été chargés depuis le fichier {nom_fichier}.
+* NULL en cas d’erreur
+*/
+Portee *cree_portee(char *nom_pere, char *nom_mere, unsigned short nb_chiots, char *nom_fichier, Date date_naissance);
