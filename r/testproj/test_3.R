0be95fc44d53554ee7a93114ade027bf8689a0f3
neclor <neclordev@gmail.com>
2025-03-26 19:10:13 +0100

    Update test_3.R

--- r/testproj/test_3.R
+++ r/testproj/test_3.R
@@ -2,13 +2,26 @@
 
 
 
+set.seed(150)
+data1<-round(rnorm(20, 10, 3), 2)
+data1
 
-a<-c(1, 2, 3 , 4, 6)
-sum(a)
+set.seed(164)
+data2<-round(rexp(25, 5), 2)
+data2
 
-iris
+set.seed(148)
+data3<-rpois(18, 2)
+data3
 
+set.seed(246)
+data4<-15-round(rchisq(67, 3))
+data4
 
-len<-iris$Sepal.Length
-tab_len<-table(len)
-tab_len
+mean(data1)
+median(data1)
+quantile(data1)
+range(data1)
+summary(data1)
+quantile(data1, 0.2)
+quantile(data1, seq(0.1, 0.9, 0.1))
