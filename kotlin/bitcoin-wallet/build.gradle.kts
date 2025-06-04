2fcaa6824fa9ac470a9c22388f15c6e7a6da7fca
neclor <neclordev@gmail.com>
2025-06-04 23:51:52 +0200

    hmm

---
+++ kotlin/bitcoin-wallet/build.gradle.kts
@@ -0,0 +1,25 @@
+plugins {
+    kotlin("jvm") version "2.1.20"
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+group = "site.neclor"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(kotlin("test"))
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+kotlin {
+    jvmToolchain(21)
+}
