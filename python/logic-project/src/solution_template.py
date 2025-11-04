8e1c154b2f4d9986a2c9613e1afc69ccdbb9b222
neclor <neclordev@gmail.com>
2025-11-04 17:35:04 +0100

    a

---
+++ python/logic-project/src/solution_template.py
@@ -0,0 +1,39 @@
+from pysat.solvers import Glucose3
+import random
+
+def question1(rows: int, columns: int, initial_row: int, initial_column: int) -> tuple[int, Glucose3, list[int]]:
+
+    size: list[int] = [rows, columns]
+
+
+
+    solver = Glucose3()
+
+	# YOUR CODE HERE
+
+	return solution, solver, variables
+
+def question3() -> int:
+	nb_sol = 0
+
+	# YOUR CODE HERE
+
+	return nb_sol
+
+def question4() -> int:
+	nb_sol = 0
+
+	# YOUR CODE HERE
+
+	return nb_sol
+
+def question5(M, N,i0,j0) -> list[list[int]]:
+	constraints = []
+
+	# YOUR CODE HERE
+
+	return constraints
+
+
+
+
